14bab600fa0760885071b20318e2ef73da648f31
Aykut Alparslan KOC <aykutalparslan@msn.com>
2022-03-11 22:40:30 +0300

    create a session manager

---
+++ Ferrite.Core/MTPtotoSession.cs
@@ -0,0 +1,32 @@
+﻿/*
+ *    Copyright 2022 Aykut Alparslan KOC <aykutalparslan@msn.com>
+ *    This file is a part of Project Ferrite
+ *
+ *    Proprietary and confidential.
+ *    Copying without express written permission is strictly prohibited.
+ */
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Ferrite.Core
+{
+    public class MTPtotoSession
+    {
+        private readonly long _id;
+        private readonly WeakReference<MTProtoConnection> _ref;
+        public MTPtotoSession(long sessionId, MTProtoConnection connection)
+        {
+            _id = sessionId;
+            _ref = new(connection);
+        }
+
+        public long SessionId => _id;
+
+        public bool TryGetConnection([NotNullWhen(true)] out MTProtoConnection? connection)
+        {
+            return _ref.TryGetTarget(out connection);
+        }
+    }
+}
+
---
+++ Ferrite.Core/SessionManager.cs
@@ -0,0 +1,34 @@
+﻿/*
+ *    Copyright 2022 Aykut Alparslan KOC <aykutalparslan@msn.com>
+ *    This file is a part of Project Ferrite
+ *
+ *    Proprietary and confidential.
+ *    Copying without express written permission is strictly prohibited.
+ */
+
+using System;
+using System.Collections.Concurrent;
+
+namespace Ferrite.Core;
+
+public class SessionManager
+{
+    private readonly ConcurrentDictionary<long, MTPtotoSession> _sessions = new();
+    public bool AddSession(long sessionId, MTPtotoSession session)
+    {
+        return _sessions.TryAdd(sessionId, session);
+    }
+    public bool RemoveSession(long sessionId)
+    {
+        return _sessions.TryRemove(sessionId, out var session);
+    }
+    public bool SessionExists(long sessionId)
+    {
+        return _sessions.ContainsKey(sessionId);
+    }
+    public bool TryGetSession(long sessionId, out MTPtotoSession session)
+    {
+        return _sessions.TryGetValue(sessionId, out session);
+    }
+}
+
