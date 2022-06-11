347b5255b9e991a12cdb4adfdef323c02df1febe
Aykut Alparslan KOC <aykutalparslan@msn.com>
2022-06-11 11:23:06 +0300

    basic schema

--- Ferrite.TLParser/Layer142Schema.cs
+++ Ferrite.TLParser/Layer142Schema.cs
@@ -23,13 +23,5 @@ public static class Layer142Schema
     public const string Schema = @"boolFalse#bc799737 = Bool;
 boolTrue#997275b5 = Bool;
 error#c4b9f9bb code:int text:string = Error;
-
-inputPeerEmpty#7f3b18ea = InputPeer;
-inputPeerSelf#7da07ec9 = InputPeer;
-inputPeerChat#35a95cb9 chat_id:long = InputPeer;
-inputPeerUser#dde8a54c user_id:long access_hash:long = InputPeer;
-inputPeerChannel#27bcbbfc channel_id:long access_hash:long = InputPeer;
-inputPeerUserFromMessage#a87b0a1c peer:InputPeer msg_id:int user_id:long = InputPeer;
-inputPeerChannelFromMessage#bd2a0840 peer:InputPeer msg_id:int channel_id:long = InputPeer;
 ";
 }
