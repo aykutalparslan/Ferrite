56c2f495f09881cf4eb72856b7a96b86cbb73387
Aykut Alparslan KOC <aykutalparslan@msn.com>
2022-06-16 18:27:23 +0300

    close #126

--- Ferrite.TL/layer139/contacts/DeleteContacts.cs
+++ Ferrite.TL/layer139/contacts/DeleteContacts.cs
@@ -20,6 +20,8 @@ using System;
 using System.Buffers;
 using DotNext.Buffers;
 using DotNext.IO;
+using Ferrite.Data;
+using Ferrite.Services;
 using Ferrite.Utils;
 
 namespace Ferrite.TL.layer139.contacts;
@@ -27,10 +29,12 @@ public class DeleteContacts : ITLObject, ITLMethod
 {
     private readonly SparseBufferWriter<byte> writer = new SparseBufferWriter<byte>(UnmanagedMemoryPool<byte>.Shared);
     private readonly ITLObjectFactory factory;
+    private readonly IContactsService _contacts;
     private bool serialized = false;
-    public DeleteContacts(ITLObjectFactory objectFactory)
+    public DeleteContacts(ITLObjectFactory objectFactory, IContactsService contacts)
     {
         factory = objectFactory;
+        _contacts = contacts;
     }
 
     public int Constructor => 157945344;
@@ -61,7 +65,64 @@ public class DeleteContacts : ITLObject, ITLMethod
 
     public async Task<ITLObject> ExecuteAsync(TLExecutionContext ctx)
     {
-        throw new NotImplementedException();
+        List<Data.InputUser> users = new();
+        foreach (var u in _id)
+        {
+            if (u is InputUserImpl inputUser)
+            {
+                users.Add(new Data.InputUser()
+                {
+                    InputUserType = InputUserType.User,
+                    UserId = inputUser.UserId,
+                    AccessHash = inputUser.AccessHash
+                });
+            }
+            else if (u is InputUserFromMessageImpl inputUserFromMessage)
+            {
+                users.Add(new Data.InputUser()
+                {
+                    InputUserType = InputUserType.UserFromMessage,
+                    UserId = inputUserFromMessage.UserId,
+                    MsgId = inputUserFromMessage.MsgId,
+                    Peer = new Data.InputPeer()
+                    {
+                        InputPeerType = inputUserFromMessage.Peer.Constructor switch
+                        {
+                            TLConstructor.InputPeerChat => InputPeerType.Chat,
+                            TLConstructor.InputPeerChannel => InputPeerType.Channel,
+                            TLConstructor.InputPeerUserFromMessage => InputPeerType.UserFromMessage,
+                            TLConstructor.InputPeerChannelFromMessage => InputPeerType.ChannelFromMessage,
+                            _ => InputPeerType.User
+                        },
+                        UserId = inputUserFromMessage.Peer.Constructor switch
+                        {
+                            TLConstructor.InputPeerUser => ((InputPeerUserImpl)inputUserFromMessage.Peer).UserId,
+                            TLConstructor.InputPeerUserFromMessage => ((InputPeerUserFromMessageImpl)inputUserFromMessage.Peer).UserId,
+                            _ => 0
+                        },
+                        AccessHash = inputUserFromMessage.Peer.Constructor switch
+                        {
+                            TLConstructor.InputPeerUser => ((InputPeerUserImpl)inputUserFromMessage.Peer).UserId,
+                            TLConstructor.InputPeerUserFromMessage =>
+                                ((InputPeerUserImpl)((InputPeerUserFromMessageImpl)inputUserFromMessage.Peer).Peer).AccessHash,
+                            _ => 0
+                        },
+                        ChatId = inputUserFromMessage.Peer.Constructor == TLConstructor.InputPeerChat
+                            ? ((InputPeerChatImpl)inputUserFromMessage.Peer).ChatId
+                            : 0,
+                        ChannelId = inputUserFromMessage.Peer.Constructor switch
+                        {
+                            TLConstructor.InputPeerChannel => ((InputPeerChannelImpl)inputUserFromMessage.Peer).ChannelId,
+                            TLConstructor.InputPeerChannelFromMessage => ((InputPeerChannelFromMessageImpl)inputUserFromMessage.Peer).ChannelId,
+                            _ => 0
+                        },
+                    }
+                });
+            }
+        }
+
+        await _contacts.DeleteContacts(ctx.AuthKeyId, users);
+        return null;
     }
 
     public void Parse(ref SequenceReader buff)
