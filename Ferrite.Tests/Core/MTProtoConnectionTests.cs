caf74ccc245ee1fe16a61bde9759e115994610bd
Aykut Alparslan KOC <aykutalparslan@msn.com>
2022-06-23 10:40:41 +0300

    fix tests

--- Ferrite.Tests/Core/MTProtoConnectionTests.cs
+++ Ferrite.Tests/Core/MTProtoConnectionTests.cs
@@ -439,7 +439,7 @@ public class MTProtoConnectionTests
             .Where(t => t.Namespace == "Ferrite.TL")
             .AsSelf();
         builder.RegisterAssemblyTypes(tl)
-            .Where(t => t.Namespace != null && t.Namespace.StartsWith("Ferrite.TL.layer139"))
+            .Where(t => t.Namespace != null && t.Namespace.StartsWith("Ferrite.TL.currentLayer"))
             .AsSelf();
         builder.Register(_ => new Int128());
         builder.Register(_ => new Int256());
