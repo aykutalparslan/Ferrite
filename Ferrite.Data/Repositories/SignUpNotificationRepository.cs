84914e5d88b3ae369986401c7b39f6025f76a988
Aykut Alparslan KOC <aykutalparslan@msn.com>
2022-09-14 14:51:30 +0300

    impl SignUpNotificationRepository

---
+++ Ferrite.Data/Repositories/SignUpNotificationRepository.cs
@@ -0,0 +1,40 @@
+// 
+// Project Ferrite is an Implementation of the Telegram Server API
+// Copyright 2022 Aykut Alparslan KOC <aykutalparslan@msn.com>
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// 
+
+namespace Ferrite.Data.Repositories;
+
+public class SignUpNotificationRepository : ISignUpNotificationRepository
+{
+    private readonly IKVStore _store;
+    public SignUpNotificationRepository(IKVStore store, IKVStore storeExported)
+    {
+        _store = store;
+        _store.SetSchema(new TableDefinition("ferrite", "signup_notifications",
+            new KeyDefinition("pk",
+                new DataColumn { Name = "user_id", Type = DataType.Long })));
+    }
+    public bool PutSignUpNotification(long userId, bool silent)
+    {
+        return _store.Put(BitConverter.GetBytes(silent), userId);
+    }
+
+    public bool GetSignUpNotification(long userId)
+    {
+        return BitConverter.ToBoolean(_store.Get(userId));
+    }
+}
