a35119895980bd9a6ff1076de04d497f69bc26f0
Aykut Alparslan KOC <aykutalparslan@msn.com>
2023-03-16 21:57:43 +0300

    add test for contacts.unblock

--- Ferrite.Tests/Integration/ContactsTests.cs
+++ Ferrite.Tests/Integration/ContactsTests.cs
@@ -206,4 +206,32 @@ fzwQPynnEsA0EyTsqtYHle+KowMhnQYpcvK/iv290NXwRjB4jWtH7tNT/PgB5tud
         var result = await client.Contacts_Block(inputUsers[0]);
         Assert.True(result);
     }
+    
+    [Fact]
+    public async Task Unblock_Returns_True()
+    {
+        using var clientContacts = new WTelegram.Client(ConfigPfs, new MemoryStream());
+        using var client = new WTelegram.Client(ConfigPfs, new MemoryStream());
+        await client.ConnectAsync();
+        var auth = await Helpers.SignUp(client, "+15555555621");
+        List<InputContact> inputContacts = new();
+        List<InputUser> inputUsers = new();
+        for (int i = 0; i < 10; i++)
+        {
+            string phoneNumber = "+90555555559" + i;
+            var a = await Helpers.SignUp(clientContacts, phoneNumber);
+            inputUsers.Add(((Auth_Authorization)a).user);
+            clientContacts.Reset();
+            InputPhoneContact c = new()
+            {
+                first_name = "bbb" + i,
+                last_name = "bbb" + i,
+                phone = phoneNumber,
+                client_id = i + 1
+            };
+            inputContacts.Add(c);
+        }
+        var result = await client.Contacts_Unblock(inputUsers[0]);
+        Assert.True(result);
+    }
 }
