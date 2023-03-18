108e6e4a5bf5c0903ffdbad80cf83efae31141fd
Aykut Alparslan KOC <aykutalparslan@msn.com>
2023-03-18 16:29:34 +0300

    add test for contacts.getBlocked

--- Ferrite.Tests/Integration/ContactsTests.cs
+++ Ferrite.Tests/Integration/ContactsTests.cs
@@ -234,4 +234,35 @@ fzwQPynnEsA0EyTsqtYHle+KowMhnQYpcvK/iv290NXwRjB4jWtH7tNT/PgB5tud
         var result = await client.Contacts_Unblock(inputUsers[0]);
         Assert.True(result);
     }
+    
+    [Fact]
+    public async Task GetBlocked_Returns_Blocked()
+    {
+        using var clientContacts = new WTelegram.Client(ConfigPfs, new MemoryStream());
+        using var client = new WTelegram.Client(ConfigPfs, new MemoryStream());
+        await client.ConnectAsync();
+        var auth = await Helpers.SignUp(client, "+15555555622");
+        List<InputContact> inputContacts = new();
+        List<InputUser> inputUsers = new();
+        for (int i = 0; i < 10; i++)
+        {
+            string phoneNumber = "+90555555560" + i;
+            var a = await Helpers.SignUp(clientContacts, phoneNumber);
+            inputUsers.Add(((Auth_Authorization)a).user);
+            clientContacts.Reset();
+            InputPhoneContact c = new()
+            {
+                first_name = "bbb" + i,
+                last_name = "bbb" + i,
+                phone = phoneNumber,
+                client_id = i + 1
+            };
+            inputContacts.Add(c);
+        }
+        var result = await client.Contacts_Block(inputUsers[0]);
+        Assert.True(result);
+        var resulBlocked = await client.Contacts_GetBlocked();
+        Assert.Single(resulBlocked.blocked);
+        Assert.Single(resulBlocked.users);
+    }
 }
